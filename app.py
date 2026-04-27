a9b524c5cdacab8f57d999e0e026b8f4995d71a6
Banty Khuraijam <banty.khuraijam@sunbirdtrust.com>
2026-04-27 16:11:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ authenticator = stauth.Authenticate(
     config["cookie"]["expiry_days"]
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login(location="main")
 
 if authentication_status:
     authenticator.logout("Logout", "sidebar")
